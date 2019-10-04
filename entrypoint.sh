95ac3b0a3d595552fb64912f098e5fc738c32b12
David Dixon <david.dixon@konsentus.com>
2019-10-04 14:37:40 +0100

    ignore local testing dir. additional tests in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -l
 
-printenv | grep GIT
-echo '---'
-cat $GITHUB_EVENT_PATH
+if [ -z $GITHUB_EVENT_PATH ] || [ ! -f $GITHUB_EVENT_PATH  ]; then
+    echo "No file containing event data found. Cannot continue"
+    exit 2
+fi
+
+JSON=$(cat $GITHUB_EVENT_PATH | jq)
+REF=$(echo -E $JSON | jq -r '.ref')
+REF_TYPE=$(echo -e $JSON | jq -r '.ref_type')
+
+if [ ! "$REF_TYPE" == "tag" ]; then
+    echo "Not a tag, skipping"
+    exit 0
+fi
+echo $JSON
+git branch -a --contains tags/${REF}
