89f4b285ce7de5bb5e757896cf746e152e07c0d4
David Dixon <david.dixon@konsentus.com>
2019-10-14 21:13:27 +0100

    add input to ignore specific branches (defaults to features and hotfixes)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,21 @@
 #!/bin/bash -l
 
+# check whether a string ($1) is in array ($2) using a grep pattern-matching search
+# eg $2="foo/* hello/* test", $1=foo/bar (success), $1=hello/world (success), $1=test123 (fail)
+is_in_pattern_list() {
+    find=$1
+    shift
+    list=("$@")
+
+    for pattern in "${list[@]}"; do
+        if echo "$find" | grep -qe "$pattern"; then
+           return 0
+        fi
+    done
+
+    return 1
+}
+
 if [ -z "${GITHUB_EVENT_PATH}" ] || [ ! -f "${GITHUB_EVENT_PATH}" ]; then
     echo "No file containing event data found. Cannot continue"
     exit 2
@@ -14,10 +30,7 @@ ORIGIN=${INPUT_REMOTE_NAME}
 JSON=$(cat ${GITHUB_EVENT_PATH} | jq)
 REF=$(echo -E ${JSON} | jq -r '.ref')
 REF_TYPE=$(echo -e ${JSON} | jq -r '.ref_type')
-
-echo $JSON
-printenv | grep GIT
-printenv | grep INPUT
+IGNORED_BRANCHES=(${INPUT_IGNORE_BRANCHES})
 
 if [ ! "${REF_TYPE}" == "tag" ]; then
     echo "Not a tag, skipping"
@@ -32,6 +45,12 @@ git config --global user.name "${GITHUB_ACTOR}"
 # since the delete trigger doesnt inform us of the exact branch the tag was on, we need to apply to all branches
 for branch in $(git for-each-ref --format="%(refname:short)" | grep "${ORIGIN}/"); do
     local_branch=${branch/$ORIGIN\//}
+
+    # skip ignored branches
+    if is_in_pattern_list $local_branch "${IGNORED_BRANCHES[@]}"; then
+        continue;
+    fi
+
     head_commit=$(git rev-list -n 1 ${branch})
 
     latest_tag=$(git describe --abbrev=0 --tags --first-parent ${branch} 2> /dev/null)
@@ -48,33 +67,33 @@ for branch in $(git for-each-ref --format="%(refname:short)" | grep "${ORIGIN}/"
 
     remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
-    # # github actions prevents itself from modifying actions, so if there have been changes to any actions
-    # # between the tags, we need to preserve the current version, and not revert!
-    # echo "${branch} : copy .github to temp location"
-    # cp -a .github /tmp/
+    # github actions prevents itself from modifying actions, so if there have been changes to any actions
+    # between the tags, we need to preserve the current version, and not revert!
+    echo "${branch} : copy .github to temp location"
+    cp -a .github /tmp/
 
-    # echo "${branch} : git checkout ${local_branch}"
-    # git checkout ${local_branch}
-    # # to revert the branch back to the last tag, we need to reset the branch and force push
-    # echo "${branch} : git reset --hard ${latest_tag}"
-    # git reset --hard ${latest_tag}
+    echo "${branch} : git checkout ${local_branch}"
+    git checkout ${local_branch}
+    # to revert the branch back to the last tag, we need to reset the branch and force push
+    echo "${branch} : git reset --hard ${latest_tag}"
+    git reset --hard ${latest_tag}
 
-    # echo "${branch} : move .github back over"
-    # rm -rf .github
-    # mv /tmp/.github .
+    echo "${branch} : move .github back over"
+    rm -rf .github
+    mv /tmp/.github .
 
-    # echo "${branch} : git add ."
-    # git add .
+    echo "${branch} : git add ."
+    git add .
 
-    # if [ -n "$(git status --porcelain)" ]; then
-    #     echo "${branch} : git commit -m 'Overlay current .github folder'"
-    #     git commit -m 'Overlay current .github folder'
-    # else
-    #     echo "${branch} : No changes detected to .github, bypassing commit"
-    # fi
+    if [ -n "$(git status --porcelain)" ]; then
+        echo "${branch} : git commit -m 'Overlay current .github folder'"
+        git commit -m 'Overlay current .github folder'
+    else
+        echo "${branch} : No changes detected to .github, bypassing commit"
+    fi
 
-    # echo "${branch} : git push --force ${remote_repo} ${local_branch}"
-    # git push --force ${remote_repo} ${local_branch}
+    echo "${branch} : git push --force ${remote_repo} ${local_branch}"
+    git push --force ${remote_repo} ${local_branch}
 done
 
 # todo:
